677c883f964d50f4585a9dbecee95369f82b3ef5
momocalc <petitlycee11235@gmail.com>
2017-08-11 20:54:47 +0900

    mod attr of moveNode. alter element to nodeid

--- src/js/bootstrap-treeview.js
+++ src/js/bootstrap-treeview.js
@@ -1300,15 +1300,15 @@
 
     /**
      * Move a node
-     * @param {Object} target_elem - target node's list element
-     * @param {Object} to_elem - bound for element ...
+     * @param {Object} node_id - moved a nodeId
+     * @param {Object} base_node_id - bound for base nodeId
      * @param {String} pos - 'above'/'below': target_elem will be moved to 'above/below' of to_elem. 'child' target_elem will be added to_elem'snodes.
      * @return {String} target could moved - 'success': moved , 'failed': failed to move
      */
-    Tree.prototype.moveNode = function (target_elem, to_elem, pos) {
+    Tree.prototype.moveNode = function (node_id, base_node_id, pos) {
         var _this = this;
-        var target_node = this.findNode(target_elem);
-        var to_base_node = this.findNode(to_elem);
+        var target_node = this.nodes[node_id];
+        var base_node = this.nodes[base_node_id];
         var SUCCESS = 'success';
         var FAILED = 'failed';
 
@@ -1327,7 +1327,7 @@
             node.parentId = parent ? parent.nodeId : undefined;
         }
 
-        if (is_child(target_node, to_base_node)) {
+        if (is_child(target_node, base_node)) {
             console.log("Error: can't move to child");
             return FAILED;
         }
@@ -1339,24 +1339,24 @@
             target_parent.nodes = null;
         }
 
-        var to_base_parent = this.getParent(to_base_node);
-        var to_nodes = to_base_parent ? to_base_parent.nodes : _this.tree;
+        var base_parent = this.getParent(base_node);
+        var base_parent_nodes = base_parent ? base_parent.nodes : _this.tree;
 
-        var idx = to_nodes.indexOf(to_base_node);
+        var idx = base_parent_nodes.indexOf(base_node);
 
         if (pos === 'above') {
-            to_nodes.splice(idx, 0, target_node);
-            set_parent(target_node, to_base_parent);
+            base_parent_nodes.splice(idx, 0, target_node);
+            set_parent(target_node, base_parent);
         } else if (pos === 'below') {
-            to_nodes.splice(idx + 1, 0, target_node);
-            set_parent(target_node, to_base_parent);
+            base_parent_nodes.splice(idx + 1, 0, target_node);
+            set_parent(target_node, base_parent);
         } else if (pos === 'child') {
             //child
-            if (!to_base_node.nodes) {
-                to_base_node.nodes = [];
+            if (!base_node.nodes) {
+                base_node.nodes = [];
             }
-            to_base_node.nodes.push(target_node);
-            set_parent(target_node, to_base_node);
+            base_node.nodes.push(target_node);
+            set_parent(target_node, base_node);
         }
         this.render();
         return SUCCESS;
